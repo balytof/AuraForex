be7e3da95b0e9b33163049b9f7d70b0b9c501db3
balytof <balytof@gmail.com>
2026-05-04 21:29:49 +0100

    feat: add script to query and display user and system settings from database

---
+++ scratch/check_settings.js
@@ -0,0 +1,13 @@
+const prisma = require('./db');
+
+async function check() {
+    console.log("--- USER SETTINGS ---");
+    const settings = await prisma.userSettings.findMany();
+    console.log(JSON.stringify(settings, null, 2));
+    
+    console.log("--- SYSTEM SETTINGS ---");
+    const sys = await prisma.systemSettings.findMany();
+    console.log(JSON.stringify(sys, null, 2));
+}
+
+check().catch(console.error).finally(() => prisma.$disconnect());
