6d34c7018873aeceb91c14b0de126b8a3700c370
balytof <balytof@gmail.com>
2026-05-04 22:28:41 +0100

    feat: add script to fetch and display all users from database

---
+++ scratch/check_users.js
@@ -0,0 +1,8 @@
+const prisma = require('./db');
+
+async function check() {
+    const users = await prisma.user.findMany();
+    console.log(JSON.stringify(users, null, 2));
+}
+
+check().catch(console.error).finally(() => prisma.$disconnect());
