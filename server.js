538bea95cc02dbb0362527f0fbb04e3fc87397b7
balytof <balytof@gmail.com>
2026-05-16 14:00:57 +0100

    chore: update robot download endpoint and filename to AuraForex_V8_INSTITUTIONAL.ex5

--- server.js
+++ server.js
@@ -82,13 +82,13 @@ app.get("/smc-forex", (req, res) => {
 });
 
 // Download do Robô
-app.get("/SMC_APEX_EA.ex5", (req, res) => {
-  const filePath = path.join(__dirname, 'public', 'SMC_APEX_EA.ex5');
+app.get("/AuraForex_V8_INSTITUTIONAL.ex5", (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'AuraForex_V8_INSTITUTIONAL.ex5');
   console.log(`[DOWNLOAD-ATTEMPT] Ficheiro: ${filePath}`);
 
   if (fs.existsSync(filePath)) {
     res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename="SMC_APEX_EA.ex5"');
+    res.setHeader('Content-Disposition', 'attachment; filename="AuraForex_V8_INSTITUTIONAL.ex5"');
     res.sendFile(filePath, (err) => {
       if (err) {
         console.error("[DOWNLOAD-ERROR]", err);
