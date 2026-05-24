531604af09f979ff592084f41f6360a8bdf3ebb5
balytof <balytof@gmail.com>
2026-05-24 17:04:00 +0100

    chore: remove temporary fix and deployment scripts

--- AuraForex_V2_Local_Execution/find_api.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let txt = fs.readFileSync('server.js', 'utf8');
-const regex = /app\.(post|get|delete)\(\"\/api\/user\/pamm\/[^\"]*\"/g;
-let match;
-while ((match = regex.exec(txt)) !== null) {
-  console.log(match[0]);
-}
