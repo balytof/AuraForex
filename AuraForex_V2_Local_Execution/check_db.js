56af28cbbaa0bd47a13117844962baeeff6d9acf
balytof <balytof@gmail.com>
2026-05-24 17:00:42 +0100

    chore: remove temporary check scripts and finish cleanup

--- AuraForex_V2_Local_Execution/check_db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany({ select: { id: true, email: true, walletBalance: true }});
-  const pamm = await prisma.pammAccount.findMany();
-  console.log("Users:", users);
-  console.log("PAMM:", pamm);
-}
-main().finally(() => prisma.$disconnect());
